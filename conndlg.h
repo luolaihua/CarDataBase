5a81169a0d2d57fafeca80ea6f4314a44b4b139f
luo <1973653885@qq.com>
2020-12-11 17:10:36 +0800

    test2

--- conndlg.h
+++ conndlg.h
@@ -1,8 +1,8 @@
-#ifndef CONNDLG_H
+﻿#ifndef CONNDLG_H
 #define CONNDLG_H
 
 #include <QDialog>
-
+class QSqlError;
 namespace Ui {
 class ConnDlg;
 }
@@ -14,6 +14,21 @@ class ConnDlg : public QDialog
 public:
     explicit ConnDlg(QWidget *parent = nullptr);
     ~ConnDlg();
+    QString driverName() const;
+    QString databaseName() const;
+    QString userName() const;
+    QString passWord() const;
+    QString hostName() const;
+    int port() const;
+    QSqlError addConnection(const QString &driver,const QString &dbName,const QString &host,const QString &user,const QString &passwd,int port = -1);
+    void creatDB();
+    void addSqliteConnection();
+
+private slots:
+    void on_okButton_clicked();
+
+    void on_cancelButton_clicked();
+    void driverChanged(const QString &);
 
 private:
     Ui::ConnDlg *ui;
